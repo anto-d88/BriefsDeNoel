61c55e360b80544b518f68936cc498b2c82a4490
anto-d88 <antonio-123@hotmail.be>
2021-12-27 16:35:55 +0100

    add section liste + keeper + monster in js and css

--- js/main.js
+++ js/main.js
@@ -15,12 +15,15 @@ function generatHeader() {
     boutonCarousel.innerHTML = naveBar.bouton01
 
     let boutonList = document.createElement("button");
+    boutonList.setAttribute("class","list")
     boutonList.innerHTML = naveBar.bouton02
 
     let boutonKeeper = document.createElement("button");
+    boutonKeeper.setAttribute("class", "keep");
     boutonKeeper.innerHTML = naveBar.bouton03
 
     let boutonMonster = document.createElement("button");
+    boutonMonster.setAttribute("class", "monst");
     boutonMonster.innerHTML = naveBar.bouton04
 
 
@@ -36,8 +39,8 @@ generatHeader();
 
 //FOOTER***********************************************************
 const info = {
-    liste01: "<li>info</li><li>info</li><li>info</li><li>info</li>",
-    liste02: "<li>info</li><li>info</li><li>info</li><li>info</li>",
+    liste01: "<li class='info'>Infos contact:</li><li>Robles Soler</li><li>Antonio</li><li>Simplon haut de france</li>",
+    liste02: "<li>07.82.76.03.31</li><li>59200</li><li>Tourcoing</li><li>antonio_robles_88@outlook.com</li>",
 }
 function generatFooter() {
 
@@ -132,14 +135,60 @@ function slidePrecedente() {
 
 precedent.addEventListener('click', slidePrecedente);
 
+//LISTE DE COURSE***************************************************
+
+let listeDeCourse = document.createElement("section");
+listeDeCourse.setAttribute("class", "li");
+
+document.body.children[1].appendChild(listeDeCourse);
+
+//KEEPER**********************************************************
+
+let keeper = document.createElement("section");
+keeper.setAttribute("class", "ke");
+
+document.body.children[1].appendChild(keeper);
+
+//MONSTER************************************************************
+
+let monster = document.createElement("section");
+monster.setAttribute("class", "mo");
+
+document.body.children[1].appendChild(monster);
+
+
 // APPEL DES DIFFERENTE SECTIONS*******************************************************
 
 const sectionCarousel = document.querySelector(".sectcar");
-// const sectionList = document.getElementById("list");
-// const sectionKeep = document.getElementById("keep");
-// const sectionMonst = document.getElementById("monst");
+const sectionList = document.querySelector(".list");
+const sectionKeep = document.querySelector(".keep");
+const sectionMonster = document.querySelector(".monst");
 
 sectionCarousel.onclick = function () {
     let sectCar = document.querySelector(".car");
+    document.querySelector(".li").style.display = "none"
+    document.querySelector(".ke").style.display = "none"
+    document.querySelector(".mo").style.display = "none"
     sectCar.style.display = "flex";
+}
+sectionList.onclick = function () {
+    let sectLi = document.querySelector(".li");
+    document.querySelector(".car").style.display = "none";
+    document.querySelector(".ke").style.display = "none";
+    document.querySelector(".mo").style.display = "none";
+    sectLi.style.display = "block";
+}
+sectionKeep.onclick = function () {
+    let sectKeep = document.querySelector(".ke");
+    document.querySelector(".car").style.display = "none";
+    document.querySelector(".li").style.display = "none";
+    document.querySelector(".mo").style.display = "none";
+    sectKeep.style.display = "block";
+}
+sectionMonster.onclick = function () {
+    let sectMonster = document.querySelector(".mo");
+    document.querySelector(".car").style.display = "none";
+    document.querySelector(".li").style.display = "none";
+    document.querySelector(".ke").style.display = "none";
+    sectMonster.style.display = "block";
 }
