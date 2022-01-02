19f7a8b60c9e46937aa094bf4e175f4a5c683b1f
anto-d88 <antonio-123@hotmail.be>
2022-01-02 01:55:44 +0100

    score keeper complete

--- js/main.js
+++ js/main.js
@@ -216,25 +216,39 @@ function generatKeeper(){
     
 let keeper = document.createElement("section");
 keeper.setAttribute("class", "ke");
-let titleKeeper = document.createElement("h2");
-titleKeeper.setAttribute("class", "titleKeeper");
+let titleKeeper = document.createElement("h1");
 titleKeeper.innerHTML = "Score Keeper";
-//AJOUTER UN INPUT DE TYPE NUMBER ICI AVEC UN ID
-let scoreJ1 = document.createElement("h3");
+let scoreJ1toJ2 = document.createElement("h3");
+scoreJ1toJ2.innerHTML = "Score:";
+let pTo = document.createElement("span");
+pTo.setAttribute("class", "to");
+pTo.innerHTML = " to ";
+let scoreJ1 = document.createElement("span");
 scoreJ1.setAttribute("id", "scoreJ1");
 scoreJ1.innerHTML = 0;
-let scoreJ2 = document.createElement("h3");
+let scoreJ2 = document.createElement("span");
 scoreJ2.setAttribute("id", "scoreJ2");
 scoreJ2.innerHTML = 0;
+let boxbutkeeper = document.createElement("section");
+boxbutkeeper.setAttribute("class", "butKeeper");
 let boutJ1 = document.createElement("button");
-boutJ1.setAttribute("onclick", "clickJ1()");
+boutJ1.setAttribute("id", "btJ1");
 boutJ1.innerHTML = "JOUEUR 1";
 let boutJ2 = document.createElement("button");
-boutJ2.setAttribute("onclick", "clickJ2()");
+boutJ2.setAttribute("id", "btJ2");
 boutJ2.innerHTML = "JOUEUR 2";
 let resetKeeper = document.createElement("button");
-resetKeeper.setAttribute("onclick", "clickRst()");
+resetKeeper.setAttribute("id", "reset");
 resetKeeper.innerHTML = "Reset";
+let scoreChoi = document.createElement("input");
+scoreChoi.setAttribute("type", "number");
+scoreChoi.setAttribute("id", "limit");
+scoreChoi.setAttribute("value", 0);
+let vinqueur = document.createElement("h3");
+vinqueur.innerHTML = "The winner is:";
+let afficheV = document.createElement("span");
+afficheV.setAttribute("id", "winner");
+afficheV.innerHTML = 0;
 
 
 
@@ -242,37 +256,101 @@ resetKeeper.innerHTML = "Reset";
 
 document.body.children[1].appendChild(keeper);
 keeper.appendChild(titleKeeper);
-keeper.appendChild(boutJ1);
-keeper.appendChild(scoreJ1);
-keeper.appendChild(scoreJ2);
-keeper.appendChild(boutJ2);
-keeper.appendChild(resetKeeper);
+keeper.appendChild(scoreChoi);
+keeper.appendChild(scoreJ1toJ2);
+scoreJ1toJ2.appendChild(scoreJ1);
+scoreJ1toJ2.appendChild(pTo);
+scoreJ1toJ2.appendChild(scoreJ2);
+keeper.appendChild(boxbutkeeper);
+boxbutkeeper.appendChild(boutJ1);
+boxbutkeeper.appendChild(resetKeeper);
+boxbutkeeper.appendChild(boutJ2);
+keeper.appendChild(vinqueur);
+vinqueur.appendChild(afficheV);
 }
 generatKeeper();
+
+
 // INCREMENTATION + RESET**************************************
 
-let scoreValue = 0;
 
-function clickJ1() {
-    scoreValue = 0;
-    keeperJ1(++scoreValue) 
-}
-function clickRst() {
-    scoreValue = 0;
-    keeperJ1(scoreValue);
-    keeperJ2(scoreValue);
-}
-function keeperJ1(valeurJ1) {
-    document.getElementById("scoreJ1").innerHTML = valeurJ1
-}
-function clickJ2() {
-    scoreValue = 0;
-    keeperJ2(++scoreValue) 
+let but1 = document.querySelector("#btJ1");
+let pl1 = document.querySelector("#scoreJ1");
+let scoreValueJ1 = 0;
+
+let but2 = document.querySelector("#btJ2");
+let pl2 = document.querySelector("#scoreJ2");
+let scoreValueJ2 = 0;
+
+let reset = document.querySelector("#reset");
+
+let inputChange = document.querySelector("#limit");
+
+let winner = document.querySelector("#winner");
+
+but1.addEventListener("click", function() {
+    let limit = document.querySelector("#limit").value;
+    scoreValueJ1++;
+    pl1.innerHTML = scoreValueJ1;
+    if( scoreValueJ1 == limit || ( scoreValueJ1>scoreValueJ2 && scoreValueJ1>limit)){
+        pl1.classList.add("green");
+        but1.setAttribute("disabled", "true");
+        but2.setAttribute("disabled", "true");
+        printWin();
+    }
+});
+
+but2.addEventListener("click", function(){
+    let limit = document.querySelector("#limit").value;
+    scoreValueJ2++;
+    pl2.innerHTML = scoreValueJ2;
+    if( scoreValueJ2 == limit || ( scoreValueJ2 > scoreValueJ1 && scoreValueJ2 > limit)){
+        pl2.classList.add("green");
+        but1.setAttribute("disabled", "true");
+        but2.setAttribute("disabled", "true");
+        printWin();
+
+    }
+});
+
+reset.addEventListener("click", function() {
+    but1.removeAttribute("disabled");
+    but2.removeAttribute("disabled");
+
+    pl1.classList.remove("green");
+    pl2.classList.remove("green");
+
+    scoreValueJ1 = 0;
+    scoreValueJ2 = 0;
+
+    pl1.innerHTML = scoreValueJ1;
+    pl2.innerHTML = scoreValueJ2;
+
+    document.querySelector("#limit").value = 0;
+    
+    winner.innerHTML = "";
+});
+
+inputChange.addEventListener("change", function () {
+    
+    pl1.innerHTML = 0;
+    pl2.innerHTML = 0;
+    
+});
+
+function printWin() {
+    
+    if( scoreValueJ1 > scoreValueJ2){
+        winner.innerHTML = "Player 1";
+        winner.classList.add("green");
+    }
+    else{
+        winner.innerHTML = "Player 2";
+        winner.classList.add("green");
+    }
 }
 
-function keeperJ2(valeurJ2) {
-    document.getElementById("scoreJ2").innerHTML = valeurJ2
-}
+
 //MONSTER************************************************************
 
 let monster = document.createElement("section");
@@ -294,21 +372,21 @@ sectionCarousel.onclick = function () {
     document.querySelector(".ke").style.display = "none"
     document.querySelector(".mo").style.display = "none"
     sectCar.style.display = "flex";
-}
+}    
 sectionList.onclick = function () {
     let sectLi = document.querySelector(".li");
     document.querySelector(".car").style.display = "none";
     document.querySelector(".ke").style.display = "none";
     document.querySelector(".mo").style.display = "none";
     sectLi.style.display = "block";
-}
+}    
 sectionKeep.onclick = function () {
     let sectKeep = document.querySelector(".ke");
     document.querySelector(".car").style.display = "none";
     document.querySelector(".li").style.display = "none";
     document.querySelector(".mo").style.display = "none";
     sectKeep.style.display = "block";
-}
+}    
 sectionMonster.onclick = function () {
     let sectMonster = document.querySelector(".mo");
     document.querySelector(".car").style.display = "none";
